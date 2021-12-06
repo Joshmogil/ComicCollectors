78f99d85ac8bb176a9a6cf595dcbe5089ba1a7eb
David Thornburg <ibdangling@gmail.com>
2021-12-06 10:04:20 -0500

    Edited Collections

--- java/src/main/java/com/techelevator/model/Collection.java
+++ java/src/main/java/com/techelevator/model/Collection.java
@@ -1,13 +1,17 @@
 package com.techelevator.model;
 
+import java.util.ArrayList;
+
 public class Collection {
 
     private long collectionId;
     private String collectionName;
+    private ArrayList<Comic> comics;
 
-    public Collection(long collectionId, String collectionName) {
+    public Collection(long collectionId, String collectionName, ArrayList<Comic> comics) {
         this.collectionId = collectionId;
         this.collectionName = collectionName;
+        this.comics = comics;
     }
 
     public long getCollectionId() {
@@ -24,5 +28,13 @@ public class Collection {
 
     public void setCollectionName(String collectionName) {
         this.collectionName = collectionName;
+    }
+
+    public ArrayList<Comic> getComics() {
+        return comics;
+    }
+
+    public void setComics(ArrayList<Comic> comics) {
+        this.comics = comics;
     }
 }
