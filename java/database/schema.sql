6fdea47a123122677a31eeb0e6036bd6980cae63
Josh Mogil <jmogil198@gmail.com>
2021-12-06 10:41:28 -0500

    Sprint 1 database design

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -21,5 +21,52 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+CREATE TABLE comics (
+
+        comic_id int,
+	title varchar(60) not null,
+	series varchar(60),
+	img_url varchar(185),
+	
+        constraint pk_comics primary key (comic_id)
+);
+
+CREATE TABLE collections (
+
+        collection_id serial,
+	user_id int not null,
+	
+        constraint pk_collections primary key (collection_id),
+        constraint fk_collections_users foreign key (user_id) references users(user_id)
+);
+
+
+CREATE TABLE collection_comic (
+
+        collection_id int not null,
+	comic_id int not null,
+	
+        constraint fk_collection_comic_collections foreign key(collection_id) references collections(collection_id),
+        constraint fk_collection_comic_comics foreign key(comic_id) references comics(comic_id)
+        
+);
+
+CREATE TABLE characters (
+
+        character_id int,
+	name varchar(60) not null,
+	
+        constraint pk_characters primary key (character_id)
+);
+
+CREATE TABLE comic_character (
+
+        comic_id int not null,
+        character_id int not null,
+        
+        constraint fk_comic_character_comics foreign key(comic_id) references comics(comic_id),
+        constraint fk_comic_character_characters foreign key(character_id) references characters(character_id)
+        
+);
 
 COMMIT TRANSACTION;
