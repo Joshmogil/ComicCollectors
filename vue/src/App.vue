6a8e8ee3414103bc8a129de654b311c76a3fdff6
Savannah Guffey <1800savannahbaines@gmail.com>
2021-12-07 12:29:53 -0500

    header

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-link v-bind:to="{ name: 'header' }">header</router-link>&nbsp;|&nbsp;
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
