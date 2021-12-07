eb84734fe53ca07d02f984f1783d6db549f23839
David Thornburg <ibdangling@gmail.com>
2021-12-07 16:39:10 -0500

    side-nav

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,10 +2,11 @@
   <div id="app">
     
     <SiteHeader></SiteHeader>
-    
+    <div class="side-nav">
           <router-link id="home" class="nav" v-bind:to="{ name: 'home' }">Home</router-link>
-          <router-link id="logging" class="nav" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+          <router-link id="logging" class="nav" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>       
           <router-link id="logging" class="nav" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    </div>
            <router-view />
 
 
@@ -20,5 +21,7 @@
   src: local("SF Comic Strip"),
  url(./src/Fonts/SFComicStrip-Bold.ttf) format("truetype");
 } */
+
+
 
 </style>
