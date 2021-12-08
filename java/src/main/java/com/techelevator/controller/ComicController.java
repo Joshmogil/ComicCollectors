88cba044a2e838dbc80b8d20fb2e75555a3e3f4c
Jim Still <jimjstill@gmail.com>
2021-12-07 22:07:00 -0500

    Final Push

--- java/src/main/java/com/techelevator/controller/ComicController.java
+++ java/src/main/java/com/techelevator/controller/ComicController.java
@@ -1,23 +1,28 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CollectionComicDao;
+import com.techelevator.dao.ComicDao;
 import com.techelevator.model.CollectionComic;
 import com.techelevator.model.Comic;
 import com.techelevator.model.MarvelComic;
 import com.techelevator.services.MarvelComicService;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
 
 
 public class ComicController {
 
     private CollectionComicDao collectionComicDao;
+    private ComicDao comicDao;
 
     public ComicController() {}
 
@@ -38,6 +43,8 @@ public class ComicController {
 
     }*/
 
+
+    //Test
     @RequestMapping(path = "test/{comicId}", method = RequestMethod.GET)//make a transfer not found exception
     public MarvelComic testGetComicId(@PathVariable long comicId){
 
@@ -46,12 +53,23 @@ public class ComicController {
         return comicFromMarvel;
     }
 
+    //Get list of comics in a collection by collectionID **NOT WORKING**
     @RequestMapping(path = "collections/{collectionId}", method = RequestMethod.GET)
     public List<Comic> getAllComicsInCollectionByCollectionId(@PathVariable long collectionId) {
 
         List<Comic> comics = collectionComicDao.getAllComicsInCollectionByCollectionId(collectionId);
         return comics;
     }
+
+    //Get single comic by id **NOT WORKING - NULL POINTER EXCEPTION**
+    @RequestMapping(path = "comics/{comicId}", method = RequestMethod.GET)
+    public Comic getSingleComicById(@PathVariable long comicId) {
+
+        Comic comic = comicDao.getSingleComicById(comicId);
+        return comic;
+    }
+
+
 
 
 
