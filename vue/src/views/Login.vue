aac95adcdd8e0a7f811884080f00935fe216ce35
David Thornburg <ibdangling@gmail.com>
2021-12-16 13:44:09 -0500

    sign in

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,17 +2,17 @@
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
       <h1 class="Promt">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
+      <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+        Invalid username and password!
+      </div>
       <div
         class="alert alert-success"
         role="alert"
         v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
+      >
+        Thank you for registering, please sign in.
+      </div>
+      <label for="username" class="sr-only"> <h2>Username</h2></label>
       <input
         type="text"
         id="username"
@@ -23,17 +23,17 @@
         autofocus
       />
       <div class="password">
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
+        <label for="password" class="sr-only"><h2>Password</h2></label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
       </div>
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
+      <router-link :to="{ name: 'register' }"><h2>Need an account?</h2></router-link>
       <button type="submit">Sign in</button>
     </form>
   </div>
@@ -49,44 +49,50 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-.text-center{
+.text-center {
   display: flex;
- justify-content: flex-start;
-
+  justify-content: flex-start;
 }
 
-.Promt{
+.Promt {
   display: flex;
   justify-content: start;
 }
+
+#login {
+  display:flex;
+  justify-content: center;
+
+}
+
 
 </style>
