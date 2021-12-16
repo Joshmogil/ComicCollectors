f04fa7ab2c7eae8173030db987200925c05d3244
Savannah Guffey <1800savannahbaines@gmail.com>
2021-12-16 11:32:36 -0500

    sidebar component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">  
-  components: { SidebarItems },
 
     <SiteHeader></SiteHeader>
+    <!-- <SidebarItems></SidebarItems> -->
     <div class="side-nav">
     </div>
       <nav class="links">
-        <sidebar-items></sidebar-items>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'login' }"><img src="@/assets/login.png" class="header-button"></router-link> 
+        </div>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'logout' }"><img src="@/assets/logout.png" class="header-button"></router-link> 
+        </div>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'home' }"><img src="@/assets/Home.png" class="header-button">
+        </router-link>
+        </div>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'userDetails', params: { userId: this.$store.state.user.id || 0 }  }"><img src="@/assets/mycollections.png" class="header-button"></router-link>
+        </div>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'statistics' }"><img src="@/assets/statistics.png" class="header-button"></router-link>
+        </div>
+        <div class= "nav-link">
+          <router-link v-bind:to="{ name: 'search' }"><img src="@/assets/search.png" class="header-button"></router-link>
+        </div>
       </nav>
          <router-view />
   </div>
 </template>
 
   <script>
-import SidebarItems from './components/SidebarItems.vue';
 
 
 
